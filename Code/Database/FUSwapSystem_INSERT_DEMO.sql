6c99693067123437e0574c9da5e87928241d742b
GiaBao <baontgse171211@fpt.edu.vn>
2024-06-19 00:41:53 +0700

    Fix FUSwapSystem_INSERT_DEMO.sql

--- Code/Database/FUSwapSystem_INSERT_DEMO.sql
+++ Code/Database/FUSwapSystem_INSERT_DEMO.sql
@@ -1,22 +1,22 @@
 ﻿INSERT INTO City(CityName) values
-('Thành phố Hà Nội');
+(N'Thành phố Hà Nội');
 
 INSERT INTO District(CityID, DistrictName) values
-(1, 'Quận Ba Đình'),
-(1, 'Quận Hoàn Kiếm');
+(1, N'Quận Ba Đình'),
+(1, N'Quận Hoàn Kiếm');
 
 INSERT INTO Ward(WardName) values
-('Phường Phúc Xá'),
-('Phường Phúc Tân');
+(N'Phường Phúc Xá'),
+(N'Phường Phúc Tân');
 
 INSERT INTO District_Ward(DistrictID, WardID) values 
 (1, 1),
 (1, 2);
 
 INSERT INTO Manager(MUserName, Password, GivenName, FamilyName, Picture, isAdmin, isAvailable, ManagerMUserName) values
-('admin', 'admin@123', 'Pham', 'Thuy', '', 1, 1, 'admin'),
-('mod1', 'mod1@123', 'Gia', 'Bao', '', 0, 1, 'admin'),
-('mod2', 'mod2@123', 'Le', 'Tuong', '', 0, 1, 'admin');
+('admin', 'admin@123', N'Phạm', N'Thủy', '', 1, 1, 'admin'),
+('mod1', 'mod1@123', N'Gia', N'Bảo', '', 0, 1, 'admin'),
+('mod2', 'mod2@123', N'Lê', N'Tưởng', '', 0, 1, 'admin');
 
 INSERT INTO Category(CateName, isAvailable, isDelete, MUserName) values
 (N'Công nghệ thông tin', 1, 0, 'admin'),
