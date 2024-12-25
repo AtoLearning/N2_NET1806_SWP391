c516b18055a2059920091ad303cca6df1e4429c2
AtoLearning <baontgse171211@fpt.edu.vn>
2024-12-25 12:16:19 +0700

    Hot fix config

--- Code/FUSwap/src/firebaseConfig.js
+++ Code/FUSwap/src/firebaseConfig.js
@@ -6,15 +6,7 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyDEWWqrIv6UaFN_tGc98xbNdCsaQBOl2Fc",
-    authDomain: "swp391-gea.firebaseapp.com",
-    projectId: "swp391-gea",
-    storageBucket: "swp391-gea.appspot.com",
-    messagingSenderId: "922875514490",
-    appId: "1:922875514490:web:db63cec04e85313fb0b5c3",
-    measurementId: "G-0ZTWKWK0Y4"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
